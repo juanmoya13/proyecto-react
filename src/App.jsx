473fa49c7cd2b58ff9f2f7ebfecda61d099892e0
Juan Moyano <juanpablo.moyano.7@gmail.com>
2025-07-16 15:20:01 -0300

    Segundo commit del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
-function App() {
-  return (
-      <h1>Hola Mundo!</h1>
+import { useEffect, useState } from "react"
 
+function App() {
+
+  const [criptos, setCriptos] = useState()
+
+  useEffect(() => {
+    fetch("https://api.coinlore.net/api/tickers/")
+      .then((resp) => resp.json())
+      .then((data) => {
+        setCriptos(data)
+      })
+      .catch(() => {
+        console.error("La petición falló")
+      })
+  },[])
+
+  return (
+    <>
+      <h1>Lista de Criptomonedas</h1>
+      <ol>
+        {criptos.map(({name, price_usd}) => (
+          <li>Nombre: {name} Precio: {price_usd}</li>
+        ))}
+      </ol>
+    </>
   )
 }
 
